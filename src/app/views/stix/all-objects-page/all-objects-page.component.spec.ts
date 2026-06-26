c499d34cca075bd57c416d68c5137eb8878207e2
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2026-06-26 12:11:50 -0400

    refactor(release-tracks): reuse all objects table for candidate selection

--- src/app/views/stix/all-objects-page/all-objects-page.component.spec.ts
+++ src/app/views/stix/all-objects-page/all-objects-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AllObjectsPageComponent } from './all-objects-page.component';
 
@@ -9,6 +10,7 @@ describe('AllObjectsPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AllObjectsPageComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
--- src/app/views/stix/all-objects-page/all-objects-page.component.ts
+++ src/app/views/stix/all-objects-page/all-objects-page.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { StixListConfig } from 'src/app/components/stix/stix-list/stix-list.component';
 
+export const ALL_OBJECTS_STIX_LIST_CONFIG: StixListConfig = {
+  showUserSearch: true,
+  excludeAttackTypes: ['relationship', 'note', 'collection'],
+};
+
 @Component({
   selector: 'app-all-objects-page',
   templateUrl: './all-objects-page.component.html',
@@ -11,8 +16,5 @@ export class AllObjectsPageComponent {
   public readonly title = 'All Objects';
   public readonly description =
     'Search and browse every object currently available in the knowledge base.';
-  public readonly stixListConfig: StixListConfig = {
-    showUserSearch: true,
-    excludeAttackTypes: ['relationship', 'note', 'collection'],
-  };
+  public readonly stixListConfig = ALL_OBJECTS_STIX_LIST_CONFIG;
 }
